f8e2674994e8bd8466fb5c2fb6675b2e3b22f616
Xuan03-THU <2955790949@qq.com>
2024-08-25 00:46:05 +0800

    改正3-gallery的问题

--- script.js
+++ script.js
@@ -1,42 +1,26 @@
-// document.getElementById("my-p").innerHTML = genRandomStr(10);
-
-// var modal = document.getElementById("myModal");
-// var img = document.getElementById("thumbnailButton").querySelector("img");
-// var span = document.getElementsByClassName("close")[0];
-
-// // 当用户点击缩略图时，打开模态框
-// img.onclick = function() {
-//     modal.style.display = "block";
-// }
-
-// // 当用户点击 "×" 符号时，关闭模态框
-// span.onclick = function() {
-//     modal.style.display = "none";
-// }
-
-// // 当用户点击任意位置外部时，关闭模态框
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
+// This code is used to create a modal window for displaying larger images.
 
 document.addEventListener('DOMContentLoaded', function() {
     const thumbnails = document.querySelectorAll('.thumbnails img');
-    const modal = document.getElementById('modal');
-    const modalContent = document.getElementById('modal-content');
+    const modal = document.getElementById('myModal');
+    const modalImg = document.querySelector('.modal-content img');
+    const span = document.getElementsByClassName("close")[0];
 
     thumbnails.forEach(thumbnail => {
         thumbnail.addEventListener('click', function() {
             const largeSrc = this.getAttribute('data-large-src');
             modal.style.display = 'block';
-            modalContent.src = largeSrc;
+            modalImg.src = largeSrc;
         });
     });
 
-    window.addEventListener('click', function(event) {
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
         if (event.target === modal) {
             modal.style.display = 'none';
         }
-    });
+    };
 });
